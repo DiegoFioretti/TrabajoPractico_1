9b408d6b059dd72ab76c68254f202fce2c6b1a7b
Diego Fioretti <diego_fioretti_97@hotmail.com>
2016-09-02 17:16:36 -0300

    sad

---
+++ source/Reg.hx
@@ -0,0 +1,17 @@
+package;
+
+/**
+ * ...
+ * @author ...
+ */
+class Reg
+{
+
+	static public var highscore:Int = 0;
+	static public var cantVidas:Int = 3;
+	static public var nombreHighscore:String = "AFC";
+	
+	inline static public var maxCantVidas = 3;
+	inline static public var velDisparo = 200;
+	inline static public var puntosOvni = 1000;
+}
